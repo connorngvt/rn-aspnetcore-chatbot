79222990cc32975f0e2494e309e00df494859f72
connorngvt <165089859+connorngvt@users.noreply.github.com>
2024-08-19 14:16:57 -0400

    update

--- ChatbotApp/backend/OpenAIApi/obj/Debug/net8.0/OpenAIApi.AssemblyInfo.cs
+++ ChatbotApp/backend/OpenAIApi/obj/Debug/net8.0/OpenAIApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenAIApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71de895bb376b52c922a445472a49d19e76e524c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32bab337d2bd2f76c9c6b836df3ace5134627591")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenAIApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenAIApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
